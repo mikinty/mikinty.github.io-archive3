328c45329505ad3515a3d06ec05ee7a092b5c639
mikinty <youmichaelc@gmail.com>
2021-10-04 21:34:06 -0400

    projects grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from "react";
 import { HashRouter as Router } from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
+import { PROJECT_GRID } from "./data.js";
 
 /**
  * This is the highest level of the web app.
  */
 class App extends React.Component {
+  createGridElement(element) {
+    return (
+      <div className="grid-element">
+        <a href={element.url} target="_blank">
+          <div
+            className="grid-picture"
+            style={{ backgroundImage: `url(${element.photo})` }}
+          />
+        </a>
+        <div className="grid-information">
+          <div className="grid-header">{element.name}</div>
+          <div className="grid-subheader">{element.description}</div>
+        </div>
+      </div>
+    );
+  }
+
+  createGrid(gridElements) {
+    let gridResult = [];
+
+    for (let i = 0; i < gridElements.length; i += 2) {
+      let rowElements =
+        i + 1 < gridElements.length
+          ? [
+              this.createGridElement(gridElements[i]),
+              this.createGridElement(gridElements[i + 1]),
+            ]
+          : this.createGridElement(gridElements[i]);
+
+      gridResult.push(<div className="grid-row">{rowElements}</div>);
+    }
+
+    return gridResult;
+  }
+
   render() {
     return (
       <Router>
@@ -26,31 +62,15 @@ class App extends React.Component {
           </div>
         </div>
         <div className="page" id="projects">
-          <div>
-            Lunar Gala: Eusociality
-          </div>
-          <div>
-            Viridity Capital LLC
-            DFCSM
-          </div>
-          <div>
-            Academia
-            CMU ECE CS Guide
-            Circuits Research
-          </div>
+          <div className="grid">{this.createGrid(PROJECT_GRID)}</div>
         </div>
         <div className="page" id="realestate">
-          Licensed Real Estate Salesperson for NY, specializing in luxury properties in Manhattan.
-
-
+          Licensed Real Estate Salesperson for NY, specializing in luxury
+          properties in Manhattan. Check out NYC LUX, a Streeteasy competitor
+          that focuses on providing high quality listings!
         </div>
         <div className="page" id="foodwine">
-          Always looking for a special way to enjoy life.
-
-          Yelp Elite
-
-          WSET II
-
+          Always looking for a special way to enjoy life. Yelp Elite WSET II
           Vivino
         </div>
       </Router>
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import LUNARGALA from "./media/lunargala.jpg";
+import DFCSM from "./media/dfcsm.jpg";
+import CMU_ECE_CS_GUIDE from "./media/cmu_ece_cs_guide.jpg";
+import UNDERSTANDING_ANALYSIS from "./media/understanding_analysis.jpg";
+
+export const PROJECT_GRID = [
+  {
+    name: "EUSOCIALITY",
+    description: "Website for the biggest fashion show in Pittsburgh.",
+    photo: LUNARGALA,
+    url: "https://2021.lunargala.org/"
+  },
+  {
+    name: "Dating for CS Majors",
+    description: "Social skills and date prep through a systematic approach.",
+    photo: DFCSM,
+    url: "https://leanpub.com/dfcsm"
+  },
+  {
+    name: "UNDERSTANDING ANALYSIS",
+    description: "Solutions for Stephen Abbott's Understanding Analysis Textbook.",
+    photo: UNDERSTANDING_ANALYSIS,
+    url: "https://github.com/mikinty/Understanding-Analysis-Abbott-Solutions"
+  },
+  {
+    name: "CMU ECE CS GUIDE",
+    description:
+      "In-depth information about how to prepare and study for classes in the CMU ECE and CS curriculum.",
+    photo: CMU_ECE_CS_GUIDE,
+    url: "https://github.com/CMU-HKN/CMU-ECE-CS-Guide"
+  },
+];
