65d79bb070126a3223b960d6d06d741279279fa0
mikinty <youmichaelc@gmail.com>
2021-10-04 22:34:14 -0400

    Add 2 more projects for hardware

--- src/data.js
+++ src/data.js
@@ -2,6 +2,8 @@ import LUNARGALA from "./media/lunargala.jpg";
 import DFCSM from "./media/dfcsm.jpg";
 import CMU_ECE_CS_GUIDE from "./media/cmu_ece_cs_guide.jpg";
 import UNDERSTANDING_ANALYSIS from "./media/understanding_analysis.jpg";
+import HUNDREDPROOF from "./media/100proof.jpg";
+import OMEGA3 from "./media/omega3.jpg";
 
 export const PROJECT_GRID = [
   {
@@ -28,5 +30,17 @@ export const PROJECT_GRID = [
       "In-depth information about how to prepare and study for classes in the CMU ECE and CS curriculum.",
     photo: CMU_ECE_CS_GUIDE,
     url: "https://github.com/CMU-HKN/CMU-ECE-CS-Guide"
+  },
+  {
+    name: "OMEGA³",
+    description: "Interact with a neural network with your hands with real-time feedback.",
+    photo: OMEGA3,
+    url: "https://github.com/mikinty/Omega3"
+  },
+  {
+    name: "100PROOF",
+    description: "Automatic bartending — order from your phone!",
+    photo: HUNDREDPROOF,
+    url: "https://github.com/eujing/100proof"
   },
 ];
